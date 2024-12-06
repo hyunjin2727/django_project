5f932542490dbd8f1abc3fb8d2ddf0dab1813518
HYUNJIN CHO <jcn09125@gmail.com>
2024-12-06 14:34:48 +0900

    Delete trip_japan directory

--- trip_japan/main/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Welcome to Trip Japan!")
-
-def fukuoka(requset):
-    return render(requset, 'Fukuoka.html')
