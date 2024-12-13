29d45975a6f94991b4a2db16ea5ad98f5f198103
HYUNJIN CHO <jcn09125@gmail.com>
2024-12-13 16:56:01 +0900

    Add files via upload

---
+++ trip_japan/main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),  # index 뷰로 요청을 처리
+    path('fukuoka/', views.fukuoka, name='fukuoka'),
+    path('tokyo/', views.tokyo, name='tokyo'),
+    path('okinawa/', views.okinawa, name='okinawa'),
+    path('osaka/', views.osaka, name='osaka'),
+    path('sapporo/', views.sapporo, name='sapporo'),
+]
